95407686c127e69eae3e721958933d17632186d2
Johanna Räihälä <johanna.raihala@gmail.com>
2021-02-02 17:42:55 +0100

    Add colours to the css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
 /** @jsxImportSource @emotion/react */
-import { css, cx } from '@emotion/react';
+import { css } from '@emotion/react';
 import mainPicture from './pictures/mainPicture.gif';
 import aboutMePicture from './pictures/aboutMePicture.png';
 import blogPicture1 from './pictures/blogPicture1.jpg';
@@ -12,23 +11,69 @@ import facebookLogo from './pictures/facebookLogo.png';
 import instagramLogo from './pictures/instagramLogo.png';
 import twitterLogo from './pictures/twitterLogo.png';
 
-const header = css`
-  margin-left: 20px;
+const headerStyle = css`
+  background-color: #d7f1e7;
+  height: 60px;
+`;
+
+const bodyStyle = css`
+  background-color: #d7f1e7;
+  text-align: center;
+  color: #008080;
+  font-family: playfair display, Georgia, Times, 'Times New Roman', serif;
+  h1 {
+    font-size: 40px;
+    font-family: ;
+  }
+  p {
+    font-size: 24;
+  }
+
+  .column {
+    color: #808080;
+  }
+
+  h2 {
+    color: #f68b8b;
+    font-weight: bold;
+  }
+
+  h3 {
+    color: #f68b8b;
+  }
+
+  button {
+    background-color: #f68b8b;
+    color: white;
+  }
+
+  a {
+    font-size: 30px;
+    color: #008080;
+  }
+`;
+
+const footerStyle = css`
+  a {
+    font-size: 12px;
+    color: #00add8;
+  }
 `;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>You're invited to...</p>
-        <img src={mainPicture} alt="the Inside Scoop" />
-        <p>
-          A free 5-day livestream event where I’ll take you on a
-          behind-the-scenes tour of my most successful passion projects!
-        </p>
-      </header>
-
-      <body>
+    <div>
+      <header css={headerStyle} />
+      <body css={bodyStyle}>
+        <section>
+          <h1>You're invited to...</h1>
+          <img src={mainPicture} alt="the Inside Scoop" />
+          <p>
+            A free 5-day livestream event where I’ll take you on a
+            behind-the-scenes tour of my most successful passion projects!
+          </p>
+        </section>
+        <hr />
         <section>
           <h2>For each project I’ll share...</h2>
           <ol>
@@ -54,15 +99,15 @@ function App() {
           </p>
           <h2>Sign up to receive reminders & video playbacks!</h2>
           <p>Email Address</p>
-          <input></input>
+          <input />
           <p>First Name</p>
-          <input></input> <br />
+          <input /> <br />
           <button>Sign me up for the scoop!</button>
         </section>
-
+        <hr />
         <section>
           <div>
-            <p>Here’s a quick look at our inside scoop schedule:</p>
+            <h2>Here’s a quick look at our inside scoop schedule:</h2>
             <img src={blogPicture1} alt="Daily Dishonesty" />
             <img src={blogPicture2} alt="Ex Boyfriend Tears" />
             <img src={blogPicture3} alt="Will Letter for Lunch" />
@@ -71,9 +116,9 @@ function App() {
           </div>
 
           <div>
-            <p>Day 1 / May 6th at 12pm EST</p>
+            <p>Day 1 </p> <p class="column">/ May 6th at 12pm EST</p>
             <a href="#1">Daily Dishonesty</a>
-            <p>
+            <p class="column">
               In 2012, I started a lettering blog that chronicled all of the
               little white lies my friends and I told in college. Little did I
               know it would go viral and land me a book deal while I was still
@@ -81,10 +126,9 @@ function App() {
               gave me the confidence to leave my full-time job to pursue
               freelance
             </p>
-
-            <p>Day 2 / May 7th at 12pm EST</p>
+            <p>Day 2 </p> <p class="column">/ May 7th at 12pm EST</p>
             <a href="#1">Ex Boyfriend Tears</a>
-            <p>
+            <p class="column">
               A particular nasty break up inspired a line of cups and flasks
               called Ex Boyfriend Tears. I was originally only planning on
               producing about 50 for fun, but they got shared like crazy
@@ -92,30 +136,27 @@ function App() {
               bought a wholesale order from me, and Urban Outfitters even ripped
               me off!
             </p>
-
-            <p>Day 3 / May 8th at 12pm EST</p>
+            <p>Day 3 </p> <p class="column">/ May 8th at 12pm EST</p>
             <a href="#1">Will Letter for Lunch</a>
-            <p>
+            <p class="column">
               I started bartering restaurant chalkboard menus for the exact menu
               items I lettered. Within 1 year, I had practiced lettering and
               eaten delicious free food at over 30 restaurants in New York City.
               Plus, I built my chalk lettering portfolio up and landed projects
               with Chobani, Samuel Adams, and Microsoft as a result.
             </p>
-
-            <p>Day 4 / May 9th at 12pm EST</p>
+            <p>Day 4 </p> <p class="column">/ May 9th at 12pm EST</p>
             <a href="#1">No Photos Please</a>
-            <p>
+            <p class="column">
               For all of 2016, I traveled and worked around the world. In order
               to spend less time looking at a screen, I challenged myself to
               document my trip in travel drawings. I lettered my way around the
               world and shared my drawings, not travel pics, on social media.
               The result? Improved drawing skills and tons of new followers!
             </p>
-
-            <p>Day 5 / May 10th at 12pm EST</p>
+            <p>Day 5 </p> <p class="column">/ May 10th at 12pm EST</p>
             <a href="#1">Flour Crowns</a>
-            <p>
+            <p class="column">
               I decided to take a pun way too far and put flours instead of
               flowers in my hair. Why? Mostly for fun, but also to become more
               associated with the food world because I knew I wanted to work on
@@ -124,9 +165,9 @@ function App() {
             </p>
           </div>
         </section>
-
+        <hr />
         <section>
-          <p>A lil' bit about me!</p>
+          <h3>A lil' bit about me!</h3>
           <img src={aboutMePicture} alt="About me" />
           <p>
             Hello! My name is Lauren Hom, and I'm a designer & letterer who's
@@ -142,8 +183,8 @@ function App() {
           <img src={twitterLogo} alt="twitter" />
           <img src={instagramLogo} alt="instagram" />
         </section>
-
-        <footer>
+        <hr />
+        <footer css={footerStyle}>
           <a href="#1">Terms of Use</a>
           <p>/</p>
           <a href="#1">Privacy Policy</a>
