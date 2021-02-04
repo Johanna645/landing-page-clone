3df7126829ed5934dc4e3563db265233b3f7b66a
Johanna Räihälä <johanna.raihala@gmail.com>
2021-02-04 16:25:44 +0100

    Add text-boxes for grid layout

--- src/App.js
+++ src/App.js
@@ -23,12 +23,22 @@ const bodyStyle = css`
   text-align: center;
   color: #008080;
   font-family: 'playfair display', Georgia, Times, 'Times New Roman', serif;
+  line-height: 1.7;
 
   h1 {
     font-size: 40px;
-    /*font-family: ;*/
+    font-weight: bold;
+
+    /*font-family: font used in template is something extra imported, so went with just cursive, which is a bit different (broader than the original);*/
+
     margin-block-start: 0;
   }
+
+  .special {
+    font-size: 24px;
+    font-weight: bold;
+  }
+
   p {
     font-size: 24;
   }
@@ -40,43 +50,78 @@ const bodyStyle = css`
   h2 {
     color: #f68b8b;
     font-weight: bold;
+    padding-top: 20px;
   }
 
   h3 {
     color: #f68b8b;
   }
 
+  a {
+    font-size: 30px;
+    font-weight: bold;
+    color: #008080;
+  }
+`;
+
+const textbox = css`
+  padding-left: 300px;
+  padding-bottom: 20px;
+  display: grid;
+  grid-template-columns: 1fr;
+  text-align: left;
+  width: 30%;
+
   input {
-    margin-bottom: 24px;
+    padding-bottom: 9px;
+    height: 36px;
   }
 
   button {
+    width: fit-content
+    text-align: center;
+    font-size: 16px;
+    font-weight: bold;
+    height: 46px;
     background-color: #f68b8b;
     color: white;
-    padding: 11 15 11 15;
+    padding: 11 15;
     :hover {
-      background-color: ;
+      background-color: #b83232;
     }
-  }
-
-  a {
-    font-size: 30px;
-    color: #008080;
-  }
 `;
 
 const columnStyle = css`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-gap: 1px;
+
   width: 50% 50%;
-  padding: 0 10px;
+`;
+
+const blogBox = css`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 300px;
+  height: 300px;
+  line-height: 1.2;
+`;
+
+const blogHeading1 = css`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const singleBlueLine = css`
+  color: #00add8;
+  margin-bottom: solid #00add8 2px;
 `;
 
 const footerStyle = css`
-  height: 120px;
+  height: 240px;
   display: inline-block;
   a {
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: lighter;
     font-size: 12px;
     color: #00add8;
   }
@@ -91,9 +136,11 @@ function App() {
       <header css={headerStyle} />
       <div css={bodyStyle}>
         <section>
-          <h1>You're invited to...</h1>
+          <h1>
+            <em>You're invited to...</em>
+          </h1>
           <img src={mainPicture} alt="the Inside Scoop" />
-          <p>
+          <p class="special">
             A free 5-day livestream event where I’ll take you on a
             behind-the-scenes tour of my most successful passion projects!
           </p>
@@ -101,127 +148,161 @@ function App() {
         <hr />
         <section>
           <h2>For each project I’ll share...</h2>
-          <ol>
+          <ul>
             <li>
-              How I came up with each idea (plus how much time + money it took
-              to bring it to life)
+              <strong>How I came up with each idea </strong>(plus how much time
+              + money it took to bring it to life)
             </li>
             <li>
-              The EXACT tangible outcomes each project led to (income, clients,
-              followers, book deal, etc.)
+              <strong>The EXACT tangible outcomes each project led to </strong>
+              (income, clients, followers, book deal, etc.)
             </li>
             <li>
-              What lessons I learned and what I’d do differently now (so you can
-              learn from my mistakes)
+              <strong>
+                What lessons I learned and what I’d do differently now{' '}
+              </strong>
+              (so you can learn from my mistakes)
             </li>
-          </ol>
+          </ul>
           <p class="column">
-            These videos will only be available to watch for *1 week* after
-            recording unless you register for this event via email, so don’t
-            delay! If you register, you'll receive a link with lifetime access
-            to the video playbacks, so you'll always have the inside scoop on my
+            <strong>
+              These videos will only be available to watch for *1 week* after
+              recording unless you register for this event via email, so don’t
+              delay!{' '}
+            </strong>
+            If you register, you'll receive a link with lifetime access to the
+            video playbacks, so you'll always have the inside scoop on my
             creative marketing strategies.
           </p>
           <h2>Sign up to receive reminders & video playbacks!</h2>
-          <p>Email Address</p>
-          <input />
-          <p>First Name</p>
-          <input /> <br />
-          <button>Sign me up for the scoop!</button>
+          <div css={textbox}>
+            <h3>Email Address</h3>
+            <input />
+            <h3>First Name</h3>
+            <input /> <br />
+            <button>Sign me up for the scoop!</button>
+          </div>
         </section>
         <hr />
         <section>
           <h2>Here’s a quick look at our inside scoop schedule:</h2>
           <div css={columnStyle}>
             <div>
-              <img src={blogPicture1} alt="Daily Dishonesty" width="300px" />
-              <img src={blogPicture2} alt="Ex Boyfriend Tears" width="300px" />
-              <img
-                src={blogPicture3}
-                alt="Will Letter for Lunch"
-                width="300px"
-              />
-              <img src={blogPicture4} alt="No Photos Please" width="300px" />
-              <img src={blogPicture5} alt="Flour Crowns" width="300px" />
+              <div css={blogBox}>
+                <img src={blogPicture1} alt="Daily Dishonesty" width="300px" />
+              </div>
+              <div css={blogBox}>
+                <img
+                  src={blogPicture2}
+                  alt="Ex Boyfriend Tears"
+                  width="300px"
+                />
+              </div>
+              <div css={blogBox}>
+                <img
+                  src={blogPicture3}
+                  alt="Will Letter for Lunch"
+                  width="300px"
+                />
+              </div>
+              <div css={blogBox}>
+                <img src={blogPicture4} alt="No Photos Please" width="300px" />
+              </div>
+              <div css={blogBox}>
+                <img src={blogPicture5} alt="Flour Crowns" width="300px" />
+              </div>
             </div>
 
             <div>
-              <span>
-                <p>
-                  Day 1 <span class="column">/ May 6th at 12pm EST</span>
+              <div css={blogBox}>
+                <span>
+                  <p css={blogHeading1}>
+                    Day 1 <span class="column">/ May 6th at 12pm EST</span>
+                  </p>
+                </span>
+
+                <a href="#1">Daily Dishonesty</a>
+
+                <p class="column">
+                  In 2012, I started a lettering blog that chronicled all of the
+                  little white lies my friends and I told in college. Little did
+                  I know it would go viral and land me a book deal while I was
+                  still in school! This is the project that launched my
+                  lettering career & gave me the confidence to leave my
+                  full-time job to pursue freelance.
                 </p>
-              </span>
+              </div>
 
-              <a href="#1">Daily Dishonesty</a>
+              <div css={blogBox}>
+                <span>
+                  <p css={blogHeading1}>
+                    Day 2 <span class="column">/ May 7th at 12pm EST</span>
+                  </p>
+                </span>
 
-              <p class="column">
-                In 2012, I started a lettering blog that chronicled all of the
-                little white lies my friends and I told in college. Little did I
-                know it would go viral and land me a book deal while I was still
-                in school! This is the project that launched my lettering career
-                & gave me the confidence to leave my full-time job to pursue
-                freelance
-              </p>
-
-              <span>
-                <p>
-                  Day 2 <span class="column">/ May 7th at 12pm EST</span>
+                <a href="#1">Ex Boyfriend Tears</a>
+                <p class="column">
+                  A particular nasty break up inspired a line of cups and flasks
+                  called Ex Boyfriend Tears. I was originally only planning on
+                  producing about 50 for fun, but they got shared like crazy
+                  online...and I ended up selling over 600 of them. Nasty Gal
+                  (RIP) bought a wholesale order from me, and Urban Outfitters
+                  even <span css={singleBlueLine}>ripped me off</span>!
                 </p>
-              </span>
+              </div>
 
-              <a href="#1">Ex Boyfriend Tears</a>
-              <p class="column">
-                A particular nasty break up inspired a line of cups and flasks
-                called Ex Boyfriend Tears. I was originally only planning on
-                producing about 50 for fun, but they got shared like crazy
-                online...and I ended up selling over 600 of them. Nasty Gal
-                (RIP) bought a wholesale order from me, and Urban Outfitters
-                even ripped me off!
-              </p>
+              <div css={blogBox}>
+                <span>
+                  <p css={blogHeading1}>
+                    Day 3 <span class="column">/ May 8th at 12pm EST</span>
+                  </p>
+                </span>
 
-              <span>
-                <p>
-                  Day 3 <span class="column">/ May 8th at 12pm EST</span>
+                <a href="#1">Will Letter for Lunch</a>
+                <p class="column">
+                  I started bartering restaurant chalkboard menus for the exact
+                  menu items I lettered. Within 1 year, I had practiced
+                  lettering and eaten delicious free food at over 30 restaurants
+                  in New York City. Plus, I built my chalk lettering portfolio
+                  up and landed projects with Chobani, Samuel Adams, and
+                  Microsoft as a result.
                 </p>
-              </span>
+              </div>
 
-              <a href="#1">Will Letter for Lunch</a>
-              <p class="column">
-                I started bartering restaurant chalkboard menus for the exact
-                menu items I lettered. Within 1 year, I had practiced lettering
-                and eaten delicious free food at over 30 restaurants in New York
-                City. Plus, I built my chalk lettering portfolio up and landed
-                projects with Chobani, Samuel Adams, and Microsoft as a result.
-              </p>
-              <span>
-                <p>
-                  Day 4 <span class="column">/ May 9th at 12pm EST</span>
-                </p>
-              </span>
+              <div css={blogBox}>
+                <span>
+                  <p css={blogHeading1}>
+                    Day 4 <span class="column">/ May 9th at 12pm EST</span>
+                  </p>
+                </span>
 
-              <a href="#1">No Photos Please</a>
-              <p class="column">
-                For all of 2016, I traveled and worked around the world. In
-                order to spend less time looking at a screen, I challenged
-                myself to document my trip in travel drawings. I lettered my way
-                around the world and shared my drawings, not travel pics, on
-                social media. The result? Improved drawing skills and tons of
-                new followers!
-              </p>
-              <span>
-                <p>
-                  Day 5 <span class="column">/ May 10th at 12pm EST</span>
+                <a href="#1">No Photos Please</a>
+                <p class="column">
+                  For all of 2016, I traveled and worked around the world. In
+                  order to spend less time looking at a screen, I challenged
+                  myself to document my trip in travel drawings. I lettered my
+                  way around the world and shared my drawings, not travel pics,
+                  on social media. The result? Improved drawing skills and tons
+                  of new followers!
                 </p>
-              </span>
-              <a href="#1">Flour Crowns</a>
-              <p class="column">
-                I decided to take a pun way too far and put flours instead of
-                flowers in my hair. Why? Mostly for fun, but also to become more
-                associated with the food world because I knew I wanted to work
-                on a cookbook in the future. This was a delicious baby step in
-                that direction without distracting from my lettering career too
-                much.
-              </p>
+              </div>
+
+              <div css={blogBox}>
+                <span>
+                  <p css={blogHeading1}>
+                    Day 5 <span class="column">/ May 10th at 12pm EST</span>
+                  </p>
+                </span>
+                <a href="#1">Flour Crowns</a>
+                <p class="column">
+                  I decided to take a pun way too far and put flours instead of
+                  flowers in my hair. Why? Mostly for fun, but also to become
+                  more associated with the food world because I knew I wanted to
+                  work on a cookbook in the future. This was a delicious baby
+                  step in that direction without distracting from my lettering
+                  career too much.
+                </p>
+              </div>
             </div>
           </div>
         </section>
@@ -229,7 +310,7 @@ function App() {
         <section>
           <h3>A lil' bit about me!</h3>
           <img src={aboutMePicture} alt="About me" />
-          <p>
+          <p class="column">
             Hello! My name is Lauren Hom, and I'm a designer & letterer who's
             used passion projects to shape her career (and teaches others how to
             do the same). In 2014, I left my 9-to-5 job in advertising to start
@@ -239,9 +320,16 @@ function App() {
             the Webby Awards. My motto is, and always will be, "Work hard, snack
             often."
           </p>
-          <img src={facebookLogo} alt="facebook" />
-          <img src={twitterLogo} alt="twitter" />
-          <img src={instagramLogo} alt="instagram" />
+          <span>
+            <img src={facebookLogo} alt="facebook" width="24px" />
+            <img
+              src={twitterLogo}
+              alt="twitter"
+              width="24px"
+              margin-right="10px"
+            />
+            <img src={instagramLogo} alt="instagram" width="24px" />
+          </span>
         </section>
         <hr />
         <footer css={footerStyle}>
